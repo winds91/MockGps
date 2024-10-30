d1a2b109431d0f263a5d40d6aec9f622cc82fe3c
cool <yangshengfei330@qq.com>
2024-10-30 19:31:32 +0800

    4.4兼容尝试

--- app/src/main/java/com/huolala/mockgps/widget/FloatingCardView.kt
+++ app/src/main/java/com/huolala/mockgps/widget/FloatingCardView.kt
@@ -1,38 +1,22 @@
 package com.huolala.mockgps.widget
 
 import android.content.Context
-import android.graphics.Outline
-import android.os.Build
 import android.util.AttributeSet
 import android.view.MotionEvent
-import android.view.View
-import android.view.ViewOutlineProvider
-import android.widget.FrameLayout
+import com.prohua.roundlayout.RoundAngleFrameLayout
 
 
 /**
  * @author jiayu.liu
  */
-class FloatingCardView : FrameLayout {
+class FloatingCardView : RoundAngleFrameLayout {
     constructor(context: Context) : super(context, null)
     constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
-    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(
         context,
         attrs,
         defStyleAttr
     )
-
-    init {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            outlineProvider = object : ViewOutlineProvider() {
-                override fun getOutline(view: View?, outline: Outline?) {
-                    outline?.setOval(0, 0, view?.width ?: 0, view?.height ?: 0)
-                }
-            }
-            clipToOutline = true
-        }
-    }
-
 
     override fun onInterceptTouchEvent(ev: MotionEvent?): Boolean {
         return true
--- common/src/main/java/com/castiel/common/AppManager.kt
+++ common/src/main/java/com/castiel/common/AppManager.kt
@@ -2,8 +2,6 @@ package com.castiel.common
 
 import android.app.Application
 import com.alibaba.android.arouter.launcher.ARouter
-import com.blankj.utilcode.util.LogUtils
-import com.tencent.bugly.crashreport.CrashReport
 import com.tencent.mmkv.MMKV
 
 class AppManager private constructor() {
@@ -23,13 +21,6 @@ class AppManager private constructor() {
         }
         this.context = context
         MMKV.initialize(context)
-        if (BuildConfig.DEBUG) {
-            ARouter.openLog()
-            ARouter.openDebug()
-        }
         ARouter.init(context)
-        CrashReport.initCrashReport(context, "8d25df392e", BuildConfig.DEBUG)
-        LogUtils.getConfig().isLogSwitch = BuildConfig.DEBUG
-        LogUtils.getConfig().globalTag = "castiel"
     }
 }
